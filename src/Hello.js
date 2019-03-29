75e60986ca473ea834632a5222e8753a11c73a56
prkkhan1 <prkkhan1@gmail.com>
2019-03-29 23:49:50 +0530

    small changes

--- src/Hello.js
+++ src/Hello.js
@@ -1,15 +1,20 @@
-import React from 'react';
+
 import React, { Component } from 'react';
 import './style.css';
-import Data from 'data';
-import firebase from 'firebase';
+import Data from './data';
+import Joi from 'joi-browser';
+
+const schema = Joi.object().keys({
+  input: Joi.string().alphanum().min(11).required()
+})
 
 
 
+export default class Hello extends Component
+{
 
-
-export default class Hello extends Component{
-
+  
+ 
 
 constructor(props){
   super(props);
@@ -25,26 +30,35 @@ constructor(props){
 }
 
 getvalue(event){
+
    this.setState({
       input:event.target.value,
     });
-    
-   
 }
 getapi(e){
+  e.preventDefault();
+
+  const result = Joi.validate({
+    input:this.state.input
+  },schema);
+  console.log(result.error);
+  if(result.error===null){
+    fetch('https://ifsc.razorpay.com/'+this.state.input)
+    .then(response =>  response.json())
+    .then((resData) => {
+    
+      this.setState({
+            output:resData,
+            isloaded:true
+              });
+      
+    })    
+  }else{
+    alert("Please Input IFSC code ");
+  }
+
  
-  fetch('https://ifsc.razorpay.com/'+this.state.input)
-.then(response =>  response.json())
-.then((resData) => {
 
-  this.setState({
-        output:resData,
-        isloaded:true
-          });
-  
-})
-
- e.preventDefault();
 }
 
 
@@ -58,8 +72,14 @@ render(){
     <div className="container-fluid" id="heading">
        <div className="col-12">
        <form className="text-center p-5">
-        <input onChange={this.getvalue} type="text" id="defaultContactFormName" className="form-control mb-4 text-center" placeholder="Enter IFSC code" />
-        <button className="btn btn-info btn button p-1 " type="submit" onClick={(e)=> this.getapi(e)}>Submit</button>
+        <input onChange={this.getvalue}
+         type="text"
+          id="defaultContactFormName"
+           className="form-control mb-4 text-center"
+            placeholder="Enter IFSC code"
+            value={this.state.input}/>
+        <button className="btn btn-info btn button p-1 " 
+        type="submit" onClick={(e)=> this.getapi(e)}>Submit</button>
         </form>
          
        </div>
